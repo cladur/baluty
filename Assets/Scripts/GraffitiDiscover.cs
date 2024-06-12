74d62ae74195b4ed3e22aa1b273e97cfdeaffc01
cladur <cl4dur@gmail.com>
2024-06-13 00:13:52 +0200

    feat: na odwal sie scenka (do revertu)

--- Assets/Scripts/GraffitiDiscover.cs
+++ Assets/Scripts/GraffitiDiscover.cs
@@ -3,9 +3,15 @@ using UnityEngine;
 public class GraffitiDiscover : MonoBehaviour
 {
     public GameObject paintPointsParentGameObject;
-    public int GraffitiPointsCount { get; set; }
+    public GameObject paintPointsParentGameObject2;
+    public GameObject paintPointsParentGameObject3;
+    public int graffitiPointsCount;
+    public int graffitiPointsCount2;
+    public int graffitiPointsCount3;
 
-    private int _stepsDone = 1;
+    private bool firstDone = false;
+    private bool secondDone = false;
+    private bool thirdDone = false;
 
     public int GraffitiPointsDiscovered
     {
@@ -13,34 +19,69 @@ public class GraffitiDiscover : MonoBehaviour
         set
         {
             _graffitiPointsDiscovered = value;
-            Debug.Log("Discovering graffiti points: " + _graffitiPointsDiscovered + "/" + GraffitiPointsCount);
+            Debug.Log("Discovering graffiti points: " + _graffitiPointsDiscovered + "/" + graffitiPointsCount);
 
             CheckStep();
         }
     }
 
-    private int _nextStep;
     private int _graffitiPointsDiscovered;
+    private float _fillThreshold = 0.8f;
 
     public Graffiti graffiti;
 
+    public GameObject firstMark;
+    public GameObject secondMark;
+    public GameObject thirdMark;
+
     private void Start()
     {
-        GraffitiPointsCount = paintPointsParentGameObject.transform.childCount;
-        Debug.Log("Point count: " + GraffitiPointsCount);
+        graffitiPointsCount = paintPointsParentGameObject.transform.childCount;
+        graffitiPointsCount2 = paintPointsParentGameObject2.transform.childCount;
+        graffitiPointsCount3 = paintPointsParentGameObject3.transform.childCount;
+        Debug.Log("Point count: " + graffitiPointsCount);
 
-        _nextStep = (int) (GraffitiPointsCount / 3.0);
-        Debug.Log("Next step: " + _nextStep);
+        graffiti.SetFillPercent(0);
+
+        paintPointsParentGameObject.SetActive(true);
+        paintPointsParentGameObject2.SetActive(false);
+        paintPointsParentGameObject3.SetActive(false);
+
+        firstMark.SetActive(true);
+        secondMark.SetActive(false);
+        thirdMark.SetActive(false);
     }
 
     private void CheckStep()
     {
-        if (_graffitiPointsDiscovered < _nextStep)
+        if (GraffitiPointsDiscovered >= _fillThreshold * graffitiPointsCount && !firstDone)
         {
-            return;
+            firstDone = true;
+            paintPointsParentGameObject.SetActive(false);
+            paintPointsParentGameObject2.SetActive(true);
+            firstMark.SetActive(false);
+            secondMark.SetActive(true);
+            graffiti.SetFillPercent(0.33f);
+            GraffitiPointsDiscovered = 0;
         }
 
-        graffiti.SetFillPercent(Mathf.Clamp(0.34f * _stepsDone++, 0f, 1f));
-        _nextStep += GraffitiPointsCount / 3;
+        if (GraffitiPointsDiscovered >= _fillThreshold * graffitiPointsCount2 && !secondDone)
+        {
+            secondDone = true;
+            paintPointsParentGameObject2.SetActive(false);
+            paintPointsParentGameObject3.SetActive(true);
+            secondMark.SetActive(false);
+            thirdMark.SetActive(true);
+            graffiti.SetFillPercent(0.66f);
+            GraffitiPointsDiscovered = 0;
+        }
+
+        if (GraffitiPointsDiscovered >= _fillThreshold * graffitiPointsCount3 && !thirdDone)
+        {
+            thirdDone = true;
+            paintPointsParentGameObject3.SetActive(false);
+            thirdMark.SetActive(false);
+            graffiti.SetFillPercent(1.0f);
+        }
     }
 }
