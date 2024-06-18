6fdfdc7be55c5c0a980805a216244952b62522e9
Mateusz Kowalski <mati12k1@gmail.com>
2024-06-18 21:23:45 +0200

    Restrict teleporatation

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -8,7 +8,48 @@ public class PlayerManager : MonoBehaviour
 
     public XRDirectInteractor leftHand;
     public XRDirectInteractor rightHand;
+
+    public XRRayInteractor leftTeleport;
+    public XRRayInteractor rightTeleport;
+
     public XRInteractionManager interactionManager;
+
+    public LayerMask groundLayerMask;
+    public InteractionLayerMask teleportMask;
+    public InteractionLayerMask noTeleportMask;
+
+    private GameObject _previousTeleportArea;
+
+    private void Update()
+    {
+        CheckFloorInteractionLayerAndSetItOnTeleport();
+    }
+
+    private void CheckFloorInteractionLayerAndSetItOnTeleport()
+    {
+        if (!Physics.Raycast(transform.position + transform.up * 0.5f, -transform.up, out RaycastHit hit, 1000, groundLayerMask))
+        {
+            return;
+        }
+
+        if (!hit.transform.parent.gameObject.TryGetComponent(typeof(TeleportationArea), out var teleportationArea))
+        {
+            return;
+        }
+
+        if (teleportationArea.transform.gameObject == _previousTeleportArea)
+        {
+            return;
+        }
+
+        if (_previousTeleportArea is not null)
+        {
+            _previousTeleportArea.GetComponent<TeleportationArea>().interactionLayers = noTeleportMask;
+        }
+
+        _previousTeleportArea = teleportationArea.transform.gameObject;
+        ((TeleportationArea)teleportationArea).interactionLayers = teleportMask;
+    }
 
     private void Awake()
     {
